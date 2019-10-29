0eef6db42889d1e977bac15303d164eb1b7cc08c
İlker Atabay <ilkeratabay@yahoo.com>
2019-10-29 22:39:52 +0300

    port update

--- server_example/server_ssl.js
+++ server_example/server_ssl.js
@@ -1,8 +1,9 @@
 // Load required modules
-var https   = require("https");     // https server core module
-var fs      = require("fs");        // file system core module
-var express = require("express");   // web framework external module
-var io      = require("socket.io"); // web socket external module
+var PORT = process.env.PORT || 8443;
+var https = require("https"); // https server core module
+var fs = require("fs"); // file system core module
+var express = require("express"); // web framework external module
+var io = require("socket.io"); // web socket external module
 
 // This sample is using the easyrtc from parent folder.
 // To use this server_example folder only without parent folder:
@@ -17,17 +18,19 @@ httpApp.use(express.static(__dirname + "/static/"));
 
 // Start Express https server on port 8443
 var webServer = https.createServer({
-    key:  fs.readFileSync(__dirname + "/certs/localhost.key"),
+    key: fs.readFileSync(__dirname + "/certs/localhost.key"),
     cert: fs.readFileSync(__dirname + "/certs/localhost.crt")
 }, httpApp);
 
 // Start Socket.io so it attaches itself to Express server
-var socketServer = io.listen(webServer, {"log level":1});
+var socketServer = io.listen(webServer, {
+    "log level": 1
+});
 
 // Start EasyRTC server
 var rtc = easyrtc.listen(httpApp, socketServer);
 
 // Listen on port 8443
-webServer.listen(8443, function () {
+webServer.listen(PORT, function () {
     console.log('listening on https://localhost:8443');
 });
